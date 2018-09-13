ebeb0d6420a89b2f92d5502ea9318e403a7bb747
user <user@JBTC10113>
2018-09-13 14:42:39 +0900

    이재현

---
+++ 자바과제/src/종합실습과제/문제08.java
@@ -0,0 +1,16 @@
+package 종합실습과제;
+
+import java.util.Scanner;
+
+public class 문제08 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.useDelimiter("\r|\n|,");
+		int a=sc.nextInt();
+		System.out.printf("정수:%d\n",a);
+		System.out.printf("절댓값:%d", a<0?-a:a);
+
+	}
+
+}
---
+++ 자바과제/src/종합실습과제/문제09.java
@@ -0,0 +1,19 @@
+package 종합실습과제;
+
+import java.util.Scanner;
+
+public class 문제09 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		System.out.printf("나이: %d\n",a);
+		if(a<=6 || a>=70)
+			System.out.printf("무료입장");
+			
+			
+			
+
+	}
+
+}
